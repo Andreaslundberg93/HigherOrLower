a29857d65a5e253d3f5dd75b5159c017bf1f3c49
andreaslundberg93 <andreaslundberg93@gmail.com>
2021-11-12 14:13:05 +0100

    Buttons

---
+++ index.js
@@ -0,0 +1,14 @@
+const button = document.getElementById("button")
+const button2 = document.getElementById("button2")
+const button3 = document.getElementById("button3")
+
+button.addEventListener("click", (event) =>{
+    event.target.innerText = "clicked!"
+
+})
+
+button.addEventListener("click", (event) =>{
+    event.target.innerText = "clicked!"
+
+})
+
