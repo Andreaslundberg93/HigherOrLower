f4b050fdb2dfe3679328e4d1e2ab56ce0b2805c5
andreaslundberg93 <andreaslundberg93@gmail.com>
2021-11-16 15:59:36 +0100

    HigherOrLowerUpdate

--- index.js
+++ index.js
@@ -1,14 +1,100 @@
-const button = document.getElementById("button")
-const button2 = document.getElementById("button2")
-const button3 = document.getElementById("button3")
+const lowerButton = document.getElementById("lower");
+const higherButton = document.getElementById("higher");
+const drawCardButton = document.getElementById("drawCard");
+const imageDiv = document.getElementById("imageDiv")
 
-button.addEventListener("click", (event) =>{
-    event.target.innerText = "clicked!"
+let deck = {};
+async function getDeck() {
+  const res = await fetch(
+    "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+  );
+  const data = await res.json();
+  console.log(data);
+  deck = data;
+  console.log(deck);
+}
 
-})
+getDeck();
 
-button.addEventListener("click", (event) =>{
-    event.target.innerText = "clicked!"
+async function drawCard() {
+  const res = await fetch(
+    "https://deckofcardsapi.com/api/deck/" + deck.deck_id + "/draw/?count=1"
+  );
+  const data = await res.json();
+  getValue(data);
+  let nyVariabel = document.createElement("img")
+    nyVariabel.src = data.cards[0].image
+    imageDiv.appendChild(nyVariabel)
 
-})
+  console.log(data.cards[0].value);
+  const cardDrawn = data.cards[0].value;
+
+  return cardDrawn;
+}
+
+drawCardButton.addEventListener("click", async () => {
+  const res = drawCard();
+
+  const image = document.getElementById("cardImage");
+
+  document.getElementById("drawCard").disabled = true;
+});
+let lowerCard;
+
+lowerButton.addEventListener("click", async () => {
+    const res = await fetch(
+        "https://deckofcardsapi.com/api/deck/" + deck.deck_id + "/draw/?count=1"
+      );
+      const data = await res.json();
+      getValue(data);
+      let nyVariabel = document.createElement("img")
+      nyVariabel.src = data.cards[0].image
+        imageDiv.appendChild(nyVariabel)
+    
+      console.log(data.cards[0].value);
+      const cardDrawn = data.cards[0].value;
+    
+      return cardDrawn;
+});
+let higherCard;
+higherButton.addEventListener("click", async () => {
+    const res = await fetch(
+        "https://deckofcardsapi.com/api/deck/" + deck.deck_id + "/draw/?count=1"
+      );
+      const data = await res.json();
+      getValue(data);
+      let nyVariabel = document.createElement("img")
+      nyVariabel.src = data.cards[0].image
+        imageDiv.appendChild(nyVariabel)
+    
+      console.log(data.cards[0].value);
+      const cardDrawn = data.cards[0].value;
+    
+      return cardDrawn;
+});
+
+async function compareCard() {
+  if (lowercard > higherCard) {
+  }
+}
+
+function getValue(data) {
+  switch (data.cards[0].value) {
+    case "ACE":
+      data.cards[0].value = 1;
+      break;
+    case "KING":
+      data.cards[0].value = 13;
+      break;
+    case "QUEEN":
+      data.cards[0].value = 12;
+      break;
+    case "JACK":
+      data.cards[0].value = 11;
+      break;
+    default:
+      data.cards[0].value = Number(data.cards[0].value);
+  }
+}
+
 
