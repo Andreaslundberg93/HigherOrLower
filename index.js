4e6d10ff9c22f37464ce73d2c4c62f0b5bf1eb46
andreaslundberg93 <andreaslundberg93@gmail.com>
2021-11-17 15:42:09 +0100

    Cleanup code

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const lowerButton = document.getElementById("lower");
 const higherButton = document.getElementById("higher");
 const drawCardButton = document.getElementById("drawCard");
-const imageDiv = document.getElementById("imageDiv")
+const imageDiv = document.getElementById("imageDiv");
 
 let startValue;
-
-
 
 let deck = {};
 async function getDeck() {
@@ -24,84 +22,56 @@ async function drawCard() {
   const res = await fetch(
     "https://deckofcardsapi.com/api/deck/" + deck.deck_id + "/draw/?count=1"
   );
-  const data = await res.json();    
+  const data = await res.json();
   getValue(data);
   startValue = data.cards[0].value;
-  
-  
 
-  let nyVariabel = document.createElement("img")
-    nyVariabel.src = data.cards[0].image
-    imageDiv.appendChild(nyVariabel)
+  let nyVariabel = document.createElement("img");
+  nyVariabel.src = data.cards[0].image;
+  imageDiv.appendChild(nyVariabel);
 
-  
   const cardDrawn = data.cards[0].value;
 
   return cardDrawn;
 }
 
 drawCardButton.addEventListener("click", async () => {
-  const res = drawCard();
-
-  const image = document.getElementById("cardImage");
+  drawCard();
 
   document.getElementById("drawCard").disabled = true;
 });
 
-
 lowerButton.addEventListener("click", async () => {
-        const res = await fetch(
-        "https://deckofcardsapi.com/api/deck/" + deck.deck_id + "/draw/?count=1"
-         );
-        const data = await res.json();
-        getValue(data);
-        let compareValue = data.cards[0].value;
-        imageDiv.children[0].setAttribute("src", data.cards[0].image)
-    
-        
+  const res = await fetch(
+    "https://deckofcardsapi.com/api/deck/" + deck.deck_id + "/draw/?count=1"
+  );
+  const data = await res.json();
+  getValue(data);
+  let compareValue = data.cards[0].value;
+  imageDiv.children[0].setAttribute("src", data.cards[0].image);
 
+  if (startValue < compareValue) {
+    alert("Game Over");
+  }
 
-        if(startValue < compareValue ){
-            alert("Game Over")
-            
-        }
-
-        startValue = data.cards[0].value;
-        
-        
-        
-        
-
-        
-      
+  startValue = data.cards[0].value;
 });
 
 higherButton.addEventListener("click", async () => {
-    const res = await fetch(
-        "https://deckofcardsapi.com/api/deck/" + deck.deck_id + "/draw/?count=1"
-        );
-        const data = await res.json();
-        getValue(data);
-        let compareValue = data.cards[0].value;
-        imageDiv.children[0].setAttribute("src", data.cards[0].image)
-        
+  const res = await fetch(
+    "https://deckofcardsapi.com/api/deck/" + deck.deck_id + "/draw/?count=1"
+  );
+  const data = await res.json();
+  getValue(data);
+  let compareValue = data.cards[0].value;
+  imageDiv.children[0].setAttribute("src", data.cards[0].image);
 
-        if(startValue > compareValue){
-            alert("Game Over")
-            
-        }
+  if (startValue > compareValue) {
+    alert("Game Over");
+  }
 
-        startValue = data.cards[0].value;
-    
-
-        
-        
-        
-           
-       
+  startValue = data.cards[0].value;
 });
-
-
 
 function getValue(data) {
   switch (data.cards[0].value) {
